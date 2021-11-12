332bafc29254d6d2539b17bde11442e4942fe770
Matheus Schneider Silva Mayrink <matheus.mayrink@prodemge.gov.br>
2021-11-12 14:05:16 -0300

    update: Header component

--- src/screens/Home/styles.tsx
+++ src/screens/Home/styles.tsx
@@ -10,4 +10,19 @@ export const Header = styled.View`
   height: 113px;
 
   background-color: ${({ theme }) => theme.colors.header};
+
+  justify-content: flex-end;
+  padding: 32px 24px;
+`;
+
+export const HeaderContent = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const TotalCars = styled.Text`
+  font-size: ${RFValue(15)}px;
+  font-family: ${({ theme }) => theme.fonts.primary_400};
+  color: ${({ theme }) => theme.colors.text};
 `;
