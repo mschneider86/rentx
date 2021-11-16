2bd68d604eb2104ab4c60ac63eec738e9628d8a8
Matheus Schneider Silva Mayrink <matheus.mayrink@prodemge.gov.br>
2021-11-16 11:00:05 -0300

    add: ImageSlider component

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BackButton } from '../../components/BackButton';
+import { ImageSlider } from '../../components/ImageSlider';
 
-import { Container, Header } from './styles';
+import { Container, Header, CarImages } from './styles';
 
 export function CarDetails() {
   return (
@@ -9,6 +10,14 @@ export function CarDetails() {
       <Header>
         <BackButton onPress={() => {}} />
       </Header>
+
+      <CarImages>
+        <ImageSlider
+          imagesUrl={[
+            'https://www.webmotors.com.br/imagens/prod/347501/AUDI_TT_RS_2.5_TFSI_GASOLINA_QUATTRO_STRONIC_34750116195765418.png?s=fill&w=275&h=183&q=70&t=true',
+          ]}
+        />
+      </CarImages>
     </Container>
   );
 }
