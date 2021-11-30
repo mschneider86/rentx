86f0f61e58201e0900d3392648f952b5dc0583bb
Matheus Schneider <matheus.ssm@gmail.com>
2021-11-30 20:11:21 -0300

    add: email input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,18 +3,21 @@ import { TextInputProps } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import { useTheme } from 'styled-components';
 
-import { Container } from './styles';
+import { Container, IconContainer, InputText } from './styles';
 
 interface InputProps extends TextInputProps {
   iconName: React.ComponentProps<typeof Feather>['name'];
 }
 
-export function Input({ iconName }: InputProps) {
+export function Input({ iconName, ...rest }: InputProps) {
   const theme = useTheme();
 
   return (
     <Container>
-      <Feather name={iconName} size={24} color={theme.colors.text_detail} />
+      <IconContainer>
+        <Feather name={iconName} size={24} color={theme.colors.text_detail} />
+      </IconContainer>
+      <InputText {...rest} />
     </Container>
   );
 }
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -24,7 +24,13 @@ export function SignIn() {
       </Header>
 
       <Form>
-        <Input iconName='mail' />
+        <Input
+          iconName='mail'
+          placeholder='E-mail'
+          keyboardType='email-address'
+          autoCorrect={false}
+          autoCapitalize='none'
+        />
       </Form>
 
       <Footer>
