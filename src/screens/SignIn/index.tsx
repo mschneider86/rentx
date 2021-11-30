2188487206564f32c8925b1597a249f966bc1557
Matheus Schneider <matheus.ssm@gmail.com>
2021-11-30 19:30:37 -0300

    add: SignIn screen

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+
+import { Button } from '../../components/Button';
+
+import { Container, Header, Title, SubTitle, Footer } from './styles';
+
+export function SignIn() {
+  return (
+    <Container>
+      <StatusBar
+        barStyle='dark-content'
+        backgroundColor='transparent'
+        translucent
+      />
+      <Header>
+        <Title>Estamos {'\n'}quase lá.</Title>
+        <SubTitle>
+          Faça seu login para começar {'\n'}uma experiência invcrível.
+        </SubTitle>
+      </Header>
+
+      <Footer>
+        <Button
+          title='Login'
+          onPress={() => {}}
+          enabled={false}
+          loading={false}
+        />
+        <Button
+          title='Criar conta gratuita'
+          onPress={() => {}}
+          enabled={false}
+          loading={false}
+        />
+      </Footer>
+    </Container>
+  );
+}
