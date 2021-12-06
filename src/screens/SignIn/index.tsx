0cd3fa185ce4a02f3a81e2b0b87ef1b8be172989
Matheus Schneider <matheus.ssm@gmail.com>
2021-12-06 17:59:04 -0300

    add: login validations

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -4,11 +4,13 @@ import {
   KeyboardAvoidingView,
   TouchableWithoutFeedback,
   Keyboard,
+  Alert,
 } from 'react-native';
 
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
 import { PasswordInput } from '../../components/PasswordInput';
+import * as Yup from 'yup';
 
 import theme from '../../styles/theme';
 
@@ -17,6 +19,28 @@ import { Container, Header, Title, SubTitle, Footer, Form } from './styles';
 export function SignIn() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  async function handleSignIn() {
+    try {
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().required('A senha é obrigatória'),
+      });
+
+      await schema.validate({ email, password });
+    } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        Alert.alert('Opa', error.message);
+      } else {
+        Alert.alert(
+          'Erro na autenticação',
+          'Ocorreu um erro ao realizar o login, verifique as credenciais'
+        );
+      }
+    }
+  }
 
   return (
     <KeyboardAvoidingView behavior='position' enabled>
@@ -56,7 +80,7 @@ export function SignIn() {
           <Footer>
             <Button
               title='Login'
-              onPress={() => {}}
+              onPress={handleSignIn}
               enabled={false}
               loading={false}
             />
