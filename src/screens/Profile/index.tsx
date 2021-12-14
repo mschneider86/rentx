489ca5184753688e797043b2f3caf94db2041599
Matheus Schneider <matheus.ssm@gmail.com>
2021-12-14 20:03:38 -0300

    add: photo selection from gallery

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -11,6 +11,7 @@ import { useTheme } from 'styled-components';
 import { useAuth } from '../../hooks/auth';
 
 import { Feather } from '@expo/vector-icons';
+import * as ImagePicker from 'expo-image-picker';
 
 import { BackButton } from '../../components/BackButton';
 import { Input } from '../../components/Input';
@@ -33,9 +34,12 @@ import {
 } from './styles';
 
 export function Profile() {
-  const [option, setOption] = useState<'dataEdit' | 'passwordEdit'>('dataEdit');
-
   const { user } = useAuth();
+
+  const [option, setOption] = useState<'dataEdit' | 'passwordEdit'>('dataEdit');
+  const [avatar, setAvatar] = useState(user.avatar);
+  const [name, setName] = useState(user.name);
+  const [driverLicense, setDriverLicense] = useState(user.driver_license);
 
   const theme = useTheme();
   const navigation = useNavigation();
@@ -48,6 +52,23 @@ export function Profile() {
 
   function handleOptionChange(selectedOption: 'dataEdit' | 'passwordEdit') {
     setOption(selectedOption);
+  }
+
+  async function handleAvatarSelection() {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 4],
+      quality: 1,
+    });
+
+    if (result.cancelled) {
+      return;
+    }
+
+    if (result.uri) {
+      setAvatar(result.uri);
+    }
   }
 
   return (
@@ -64,8 +85,8 @@ export function Profile() {
             </HeaderTop>
 
             <PhotoContainer>
-              <Photo source={{ uri: 'https://github.com/mschneider86.png' }} />
-              <PhotoButton onPress={() => {}}>
+              {!!avatar && <Photo source={{ uri: avatar }} />}
+              <PhotoButton onPress={handleAvatarSelection}>
                 <Feather name='camera' size={24} color={theme.colors.shape} />
               </PhotoButton>
             </PhotoContainer>
@@ -96,6 +117,7 @@ export function Profile() {
                   placeholder='Nome'
                   autoCorrect={false}
                   defaultValue={user.name}
+                  onChangeText={setName}
                 />
                 <Input
                   iconName='mail'
@@ -107,6 +129,7 @@ export function Profile() {
                   placeholder='CNH'
                   keyboardType='numeric'
                   defaultValue={user.driver_license}
+                  onChangeText={setDriverLicense}
                 />
               </Section>
             ) : (
