d4b2d61f9d34a79a8c0343362e0fa53b15f51e0c
Matheus Schneider <matheus.ssm@gmail.com>
2021-12-18 13:11:14 -0300

    fix: added loading while routes are being loaded

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -24,6 +24,7 @@ interface AuthContextData {
   signIn: (credentials: SignInCredentials) => Promise<void>;
   signOut: () => Promise<void>
   updateUser: (user: User) => Promise<void>;
+  loading: boolean;
 }
 
 interface AuthProviderProps {
@@ -34,6 +35,7 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps) {
   const [data, setData] = useState<User>({} as User);
+  const [loading,setLoading] = useState(true);
 
   async function signIn({ email, password }: SignInCredentials) {
     try {
@@ -105,6 +107,7 @@ function AuthProvider({ children }: AuthProviderProps) {
         const userData = response[0]._raw as unknown as User;
         api.defaults.headers.authorization = `Bearer ${userData.token}`
         setData(userData)
+        setLoading(false);
       }
     }
     loadUserData();
@@ -116,7 +119,8 @@ function AuthProvider({ children }: AuthProviderProps) {
         user: data,
         signIn,
         signOut,
-        updateUser
+        updateUser,
+        loading
       }}
     >
       {children}
